b1c0e35779ad9f4a17633e2335e25cd3729149dd
codu2 <khub0929@gmail.com>
2022-11-02 23:53:14 +0900

    expo-auth-session의 AuthSession을 이용한 redirect_uri, startAsync로 user access token 생성 및 twitch로 로그인하기 기능 추가

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -9,10 +9,31 @@ import {
 import React, { useState } from "react";
 import tw from "twrnc";
 import { useNavigation } from "@react-navigation/native";
+import { CLIENT_ID, CLIENT_SECRET } from "@env";
+import * as AuthSession from "expo-auth-session";
 
 const LoginScreen = () => {
   const [selected, setSelected] = useState("login");
   const navigation = useNavigation();
+
+  const signInWithTwitch = async () => {
+    const redirect_url = AuthSession.getRedirectUrl();
+    const authUrl = `https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${redirect_url}&navigation&scope=user%3Aread%3Aemail+user%3Aread%3Afollows+bits%3Aread&force_verify=true`;
+
+    const { type, params } = await AuthSession.startAsync({ authUrl: authUrl });
+
+    if (type === "success") {
+      const { code } = params;
+      const getUserToken = await fetch(
+        `https://id.twitch.tv/oauth2/token?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&code=${code}&grant_type=authorization_code&redirect_uri=${redirect_url}`,
+        {
+          method: "POST",
+        }
+      );
+      const data = await getUserToken.json();
+      console.log(data);
+    }
+  };
 
   return (
     <View style={tw`flex flex-1 bg-black`}>
@@ -43,14 +64,14 @@ const LoginScreen = () => {
         </View>
 
         <ScrollView
-          contentContainerStyle={tw`w-full flex items-center mt-6 pb-28`}
+          contentContainerStyle={tw`w-full flex items-center mt-4 pb-28`}
         >
           <View>
             <Text style={tw`text-base font-semibold text-gray-300`}>
               아이디
             </Text>
             <TextInput
-              style={tw`w-80 h-10 bg-transparent border border-[#8758FF] rounded-lg mt-2 mb-6 px-2 py-2 text-white`}
+              style={tw`w-80 h-10 bg-transparent border border-[#8758FF] rounded-lg mt-2 mb-4 px-2 py-2 text-white`}
               autoComplete="false"
               placeholderTextColor={"#ccc"}
             />
@@ -60,7 +81,7 @@ const LoginScreen = () => {
               비밀번호
             </Text>
             <TextInput
-              style={tw`w-80 h-10 bg-transparent border border-[#8758FF] rounded-lg mt-2 mb-6 px-2 py-2 text-white`}
+              style={tw`w-80 h-10 bg-transparent border border-[#8758FF] rounded-lg mt-2 mb-4 px-2 py-2 text-white`}
               autoComplete="false"
               placeholderTextColor={"#ccc"}
             />
@@ -91,6 +112,7 @@ const LoginScreen = () => {
                 <Text style={tw`text-base font-semibold text-gray-300`}>
                   전화번호
                   <Text style={tw`text-sm font-light`}>
+                    {" "}
                     (인증이 필요합니다)
                   </Text>
                 </Text>
@@ -103,18 +125,31 @@ const LoginScreen = () => {
             </>
           )}
 
-          <TouchableOpacity
-            style={tw`w-64 bg-[#181818] py-3 mt-6 mb-4 flex items-center justify-center rounded-xl`}
-          >
-            <Text style={tw`text-gray-200 font-bold text-base`}>
-              {selected === "login" ? "로그인" : "회원가입"}
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => navigation.navigate("navigation")}>
-            <Text style={tw`text-[#8758FF] text-sm py-2`}>
-              비회원으로 이용하기
-            </Text>
-          </TouchableOpacity>
+          <View style={tw`mt-2 mb-4`}>
+            <TouchableOpacity
+              style={tw`w-64 bg-[#181818] py-3 my-2 flex items-center justify-center rounded-lg`}
+            >
+              <Text style={tw`text-gray-200 font-bold text-base`}>
+                {selected === "login" ? "로그인" : "회원가입"}
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={signInWithTwitch}
+              style={tw`w-64 bg-[#8758FF] py-3 my-2 flex items-center justify-center rounded-lg`}
+            >
+              <Text style={tw`text-gray-200 font-bold text-base`}>
+                Twitch로 로그인하기
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("navigation")}
+              style={tw`my-2`}
+            >
+              <Text style={tw`text-[#8758FF] text-sm text-center`}>
+                비회원으로 이용하기
+              </Text>
+            </TouchableOpacity>
+          </View>
         </ScrollView>
       </View>
     </View>
